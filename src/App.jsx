3e82c7f9c22369fd2dcd7bb87b2163c41e7b7b54
ylana98787 <ylana98787@gmail.com>
2026-05-05 20:56:01 +0300

    feat: додано маршрутизацію для сторінки бронювання

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import BookingPage from './pages/BookingPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/booking/:trainId" element={<BookingPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
